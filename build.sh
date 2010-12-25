c19fc68e9a252e49caa56b9e5b2f916cbf112563
Pepijn de Vos <pepijndevos@gmail.com>
2010-12-25 12:57:00 +0100

    improved build script

--- build.sh
+++ build.sh
@@ -2,9 +2,21 @@
 
 compass create wordpress --using compass/extension --syntax sass
 svn export http://thematic.googlecode.com/svn/trunk/ thematic
+
 cp -r thematic/thematicsamplechildtheme/ wordpress/templates/project/
 echo "discover :all" >> wordpress/templates/project/manifest.rb
+sed -i '~' '/*\//,$ d' wordpress/templates/project/style.css
+echo "*/
+
+@import url('stylesheets/screen.css');" >> wordpress/templates/project/style.css
+
 sass-convert --from css --to sass --recursive thematic/library wordpress/stylesheets
+
+for file in wordpress/stylesheets/*/*.sass
+do
+	mv $file `echo $file | sed -E "s/([^\/]*)$/_\\1/"`
+done
+
 find wordpress/stylesheets/*/* | \
 	sed -E "s/.*\/([^\/]*\/[^\/]*\.sass)$/@import '\\1'/" >> \
 	wordpress/stylesheets/_wordpress.sass
